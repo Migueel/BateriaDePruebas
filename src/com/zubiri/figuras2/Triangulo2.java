a3af662ea2766bd903eae73359343ab3c23b9ab7
Migueel <mgilav14dw@ikzubirimanteo.com>
2015-05-30 19:01:47 +0200

    -

---
+++ src/com/zubiri/figuras2/Triangulo2.java
@@ -0,0 +1,28 @@
+package com.zubiri.figuras2;
+
+public class Triangulo2 extends FiguraGeometrica2 {
+
+	public Triangulo2(){};
+	
+	public Triangulo2(double base, double altura) {
+		setNombre("Triangulo");
+		setBase(base);
+		setAltura(altura);		
+	}
+
+	@Override public double area(){
+		double area = getBase()*getAltura()/2;
+		return area;
+	}
+	//Triángulo equilatero
+	@Override public double perimetro() {
+		double peri = getBase()*3;
+		return peri;
+	}
+	
+	public void mostrar() {
+	    super.mostrar();
+		System.out.println("\nEl area del triangulo es: " + area());
+		System.out.println("\nEl perimetro del triangulo es: " + perimetro());
+	}
+}
